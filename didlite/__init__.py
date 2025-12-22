2b0dfa2bd7c08ed394e047ab9123962dbaa6bd66
jondepalma <jon@jondepalma.net>
2025-12-22 05:14:12 +0000

    Committing initial application components

---
+++ didlite/__init__.py
@@ -0,0 +1,18 @@
+"""
+didlite: Lightweight Identity for Agents & IoT
+"""
+
+__version__ = "0.1.0"
+__author__ = "Your Name / Org"
+
+# Expose the main classes to the top level
+from .core import AgentIdentity, resolve_did_to_key
+from .jws import create_jws, verify_jws
+
+# Define what happens when someone does `from didlite import *`
+__all__ = [
+    "AgentIdentity", 
+    "resolve_did_to_key", 
+    "create_jws", 
+    "verify_jws"
+]
